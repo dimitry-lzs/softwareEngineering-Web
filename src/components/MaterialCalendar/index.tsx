24ab60e448aaf9c2110c0483b5db51dfa8832a95
nae64 <xdan2805@gmail.com>
2025-05-11 18:04:03 +0300

    initial calendar page

---
+++ src/components/MaterialCalendar/index.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { DateCalendar } from "@mui/x-date-pickers";
+
+
+export default function MaterialCalendar() {
+    const materialTheme = createTheme();    
+    
+        return (
+    
+            <ThemeProvider theme={materialTheme}>
+                <DateCalendar/>
+            </ThemeProvider>
+        );
+}
