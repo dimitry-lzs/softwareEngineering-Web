e67a8cabf32830c1b6e1f7e35b6566f0fbf23d57
Tarmac <iwannandrianou@gmail.com>
2025-05-14 14:23:44 +0300

    profile working

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,41 +1,25 @@
-import AspectRatio from "@mui/joy/AspectRatio";
-import Box from "@mui/joy/Box";
-import Button from "@mui/joy/Button";
-import Divider from "@mui/joy/Divider";
-import FormControl from "@mui/joy/FormControl";
-import FormLabel from "@mui/joy/FormLabel";
-import Input from "@mui/joy/Input";
-import IconButton from "@mui/joy/IconButton";
-import Stack from "@mui/joy/Stack";
-import Typography from "@mui/joy/Typography";
-import Card from "@mui/joy/Card";
-import CardActions from "@mui/joy/CardActions";
-import CardOverflow from "@mui/joy/CardOverflow";
 import EmailRoundedIcon from "@mui/icons-material/EmailRounded";
 import EditRoundedIcon from "@mui/icons-material/EditRounded";
 import PhoneIcon from "@mui/icons-material/Phone";
 import AssignmentIndIcon from "@mui/icons-material/AssignmentInd";
 import SectionTitle from "../../components/SectionTitle";
 import { useState } from "react";
+import { AspectRatio, Box, Button, Card, CardActions, CardOverflow, Divider, FormControl, FormLabel, IconButton, Input, Stack, Typography } from "@mui/joy";
 
 export default function Profile() {
-    // const [disabled, setDisabled] = useState(true);
-    const [formValues] = useState({
-        firstName: "Chuck",
-        lastName: "Norris",
-        phoneNumber: "69999999999",
-        amka: "09030224674",
-        email: "chucknorrispushestheearth@test.com",
-        createDate: "2023-10-01",
-    });
+    const [disabled, setDisabled] = useState(true);
 
-    // const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    //     event.preventDefault();
-    //     const formData = new FormData(event.currentTarget);
-    //     const formJson = Object.fromEntries((formData as any).entries());
-    //     alert(JSON.stringify(formJson));
-    //     // setDisabled(true);
-    // };
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const formJson = Object.fromEntries((formData as any).entries());
+        console.log("Form submitted: ", formJson);
+        setDisabled(true);
+    };
+
+    const handleChange = () => {
+        disabled && setDisabled(false);
+    };
 
     return (
         <Box sx={{ flex: 1, width: "100%" }}>
@@ -59,12 +43,7 @@ export default function Profile() {
                     </Box>
                     <Divider />
                     <form
-                        onSubmit={(event) => {
-                            event.preventDefault();
-                            const formData = new FormData(event.currentTarget);
-                            const formJson = Object.fromEntries((formData as any).entries());
-                            alert(JSON.stringify(formJson));
-                        }}
+                        onSubmit={handleSubmit}
                     >
                         <Stack
                             direction="row"
@@ -122,7 +101,8 @@ export default function Profile() {
                                             name="firstName"
                                             size="sm"
                                             placeholder="First name"
-                                            // defaultValue={formValues.firstName}
+                                            onChange={handleChange}
+                                            required
                                         />
                                     </FormControl>
                                     <FormControl
@@ -135,7 +115,7 @@ export default function Profile() {
                                             name="lastName"
                                             size="sm"
                                             placeholder="Last name"
-                                            // defaultValue={formValues.lastName}
+                                            onChange={handleChange}
                                             sx={{ flexGrow: 1 }}
                                         />
                                     </FormControl>
@@ -147,7 +127,8 @@ export default function Profile() {
                                             name="phoneNumber"
                                             type="tel"
                                             size="sm"
-                                            // defaultValue={formValues.phoneNumber}
+                                            placeholder="Phone Number"
+                                            onChange={handleChange}
                                             startDecorator={<PhoneIcon />}
                                         />
                                     </FormControl>
@@ -156,7 +137,8 @@ export default function Profile() {
                                         <Input
                                             name="amka"
                                             size="sm"
-                                            // defaultValue={formValues.amka}
+                                            placeholder="AMKA"
+                                            onChange={handleChange}
                                             startDecorator={<AssignmentIndIcon />}
                                         />
                                     </FormControl>
@@ -173,8 +155,8 @@ export default function Profile() {
                                             size="sm"
                                             type="email"
                                             startDecorator={<EmailRoundedIcon />}
-                                            placeholder="email"
-                                            // defaultValue={formValues.email}
+                                            placeholder="Email"
+                                            onChange={handleChange}
                                             sx={{ width: "255px" }} // adjust the width as needed
                                         />
                                     </FormControl>
@@ -183,122 +165,19 @@ export default function Profile() {
                                         color="neutral"
                                         sx={{ pr: 2, pb: 1 }}
                                     >
-                                        Member since {formValues.createDate}
+                                        Member since 2023-10-01
                                     </Typography>
                                 </Stack>
                             </Stack>
-                        </Stack>
-                        <Stack
-                            direction="column"
-                            spacing={2}
-                            sx={{ display: { xs: "flex", md: "none" }, my: 1 }}
-                        >
-                            <Stack direction="row" spacing={2}>
-                                <Stack direction="column" spacing={1}>
-                                    <AspectRatio
-                                        ratio="1"
-                                        maxHeight={108}
-                                        sx={{ flex: 1, minWidth: 108, borderRadius: "100%" }}
-                                    >
-                                        <img
-                                            src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286"
-                                            srcSet="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286&dpr=2 2x"
-                                            loading="lazy"
-                                            alt=""
-                                        />
-                                    </AspectRatio>
-                                    <IconButton
-                                        aria-label="upload new picture"
-                                        size="sm"
-                                        variant="outlined"
-                                        color="neutral"
-                                        sx={{
-                                            bgcolor: "background.body",
-                                            position: "absolute",
-                                            zIndex: 2,
-                                            borderRadius: "50%",
-                                            left: 85,
-                                            top: 180,
-                                            boxShadow: "sm",
-                                        }}
-                                    >
-                                        <EditRoundedIcon />
-                                    </IconButton>
-                                </Stack>
-                                <Stack spacing={1} sx={{ flexGrow: 1 }}>
-                                    <FormLabel>Name</FormLabel>
-                                    <FormControl
-                                        sx={{
-                                            display: {
-                                                sm: "flex-column",
-                                                md: "flex-row",
-                                            },
-                                            gap: 2,
-                                        }}
-                                    >
-                                        <Input
-                                            name="firstName"
-                                            size="sm"
-                                            placeholder="First name"
-                                            // defaultValue={formValues.firstName}
-                                        />
-                                    </FormControl>
-                                    <FormControl
-                                        sx={{
-                                            display: {
-                                                sm: "flex-column",
-                                                md: "flex-row",
-                                            },
-                                            gap: 2,
-                                        }}
-                                    >
-                                        <Input
-                                            name="lastName"
-                                            size="sm"
-                                            placeholder="Last name"
-                                            // defaultValue={formValues.lastName}
-                                        />
-                                    </FormControl>
-                                </Stack>
-                            </Stack>
-                            <FormControl>
-                                <FormLabel>Phone Number</FormLabel>
-                                <Input
-                                    name="phoneNumber"
-                                    type="tel"
-                                    size="sm"
-                                    // defaultValue={formValues.phoneNumber}
-                                />
-                            </FormControl>
-                            <FormControl>
-                                <FormLabel>AMKA</FormLabel>
-                                <Input
-                                    name="amka"
-                                    size="sm"
-                                    // defaultValue={formValues.amka}
-                                />
-                            </FormControl>
-                            <FormControl sx={{ flexGrow: 1 }}>
-                                <FormLabel>Email</FormLabel>
-                                <Input
-                                    name="email"
-                                    size="sm"
-                                    type="email"
-                                    startDecorator={<EmailRoundedIcon />}
-                                    placeholder="email"
-                                    // defaultValue={formValues.email}
-                                    sx={{ flexGrow: 1 }}
-                                />
-                            </FormControl>
                         </Stack>
                         <CardOverflow sx={{ borderTop: "1px solid", borderColor: "divider" }}>
                             <CardActions sx={{ alignSelf: "flex-end", pt: 2 }}>
                                 <Button
                                     size="sm"
                                     variant="solid"
-                                    // disabled={disabled}
                                     type="submit"
                                     startDecorator={<EditRoundedIcon />}
+                                    disabled={disabled}
                                 >
                                     Save
                                 </Button>
