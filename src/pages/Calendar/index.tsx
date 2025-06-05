d6e0b406ed21e5cafae8c16c6a2b4a59fe51185f
Tarmac <iwannandrianou@gmail.com>
2025-06-05 18:01:28 +0300

    merge mistake: calendar

--- src/pages/Calendar/index.tsx
+++ src/pages/Calendar/index.tsx
@@ -1,24 +1,55 @@
-import { Divider, Stack, Typography } from '@mui/joy';
-import UpcomingAppointments from '../../components/UpcomingAppointments';
+import MaterialCalendar from "../../components/MaterialCalendar";
+import { Box, Divider, Stack, Typography } from "@mui/joy";
+import UpcomingAppointments from "../../components/UpcomingAppointments";
 
 export default function Calendar() {
+
     return (
-        <Typography level='h2' sx={{ mb: 2 }}>
-            {' '}
-            Calendar
-            <Stack
-                direction='row'
-                spacing={2}
-                divider={<Divider orientation='vertical' />}
+        <Typography level="h2" sx={{ mb: 2 }}
+        > Calendar
+            <Box
                 sx={{
+                    display: 'flex',
+                    //direction: 'row',
+                    //minWidth: '100%',
                     justifyContent: 'center',
                     alignItems: 'center',
+                    minHeight: '100%',
                     flexWrap: 'wrap',
-                    gap: 2,
+                    marginTop: 2
+                    //padding: 1,
                 }}
             >
-                <UpcomingAppointments />
-            </Stack>
+                <Stack
+                    direction="row"
+                    spacing={2}
+                    divider={<Divider
+                        orientation="vertical"
+                        sx={{
+                            height: "auto", // Allow the divider to stretch dynamically
+                            //alignSelf: "stretch", // Stretch the divider to match sibling height
+                        }}
+                    />}
+                    sx={{
+                        justifyContent: 'center',
+                        alignItems: 'stretch',
+                        gap: 4,
+                    }}
+                >
+                    <MaterialCalendar />
+
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'space-between',
+                            height: '100%',
+                        }}
+                    >
+                        <UpcomingAppointments />
+                    </Box>
+                </Stack>
+            </Box>
         </Typography>
     );
 }
