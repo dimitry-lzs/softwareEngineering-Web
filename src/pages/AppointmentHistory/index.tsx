a3850f6675e42638009176ad7d433b738e624f52
dimitry-lzs <lazanasdimitrios@gmail.com>
2025-06-11 13:31:02 +0300

    fix: add cursor pointer style to appointment rows and remove pagination component

--- src/pages/AppointmentHistory/index.tsx
+++ src/pages/AppointmentHistory/index.tsx
@@ -133,7 +133,7 @@ export default function AppointmentHistory() {
                                 });
                                 return (
 
-                                    <tr key={appointment.appointmentid} onClick={() => navigate(`/history/${appointment.appointmentid}`)}>
+                                    <tr style={{ cursor: 'pointer' }} key={appointment.appointmentid} onClick={() => navigate(`/history/${appointment.appointmentid}`)}>
                                         <td>
                                             <Typography level="body-xs">A25-{appointment.appointmentid}</Typography>
                                         </td>
@@ -187,48 +187,6 @@ export default function AppointmentHistory() {
                     </tbody>
                 </Table>
             </Sheet>
-            <Box
-                className="Pagination-laptopUp"
-                sx={{
-                    pt: 2,
-                    gap: 1,
-                    [`& .${iconButtonClasses.root}`]: { borderRadius: '50%' },
-                    display: {
-                        xs: 'none',
-                        md: 'flex',
-                    },
-                }}
-            >
-                <Button
-                    size="sm"
-                    variant="outlined"
-                    color="neutral"
-                    startDecorator={<KeyboardArrowLeftIcon />}
-                >
-                    Previous
-                </Button>
-
-                <Box sx={{ flex: 1 }} />
-                {['1', '2', '3', '…', '8', '9', '10'].map((page) => (
-                    <IconButton
-                        key={page}
-                        size="sm"
-                        variant={Number(page) ? 'outlined' : 'plain'}
-                        color="neutral"
-                    >
-                        {page}
-                    </IconButton>
-                ))}
-                <Box sx={{ flex: 1 }} />
-                <Button
-                    size="sm"
-                    variant="outlined"
-                    color="neutral"
-                    endDecorator={<KeyboardArrowRightIcon />}
-                >
-                    Next
-                </Button>
-            </Box>
         </React.Fragment>
     );
 }
